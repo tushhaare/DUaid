1221ca251036e85bd94cf51f7482f8327991526c
tushhaare <164902897+tushhaare@users.noreply.github.com>
2024-12-21 10:10:55 +0530

    Delete search.js

--- search.js
+++
@@ -1,7 +0,0 @@
-console.log("JavaScript is loaded!");
-
-// Add a simple test for interaction
-document.getElementById('search-bar').addEventListener('input', function () {
-  console.log("Input value:", this.value);
-  document.getElementById('results').innerHTML = `<li>Searching for: ${this.value}</li>`;
-});
